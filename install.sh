bc81f3ddcbd6a9f21c2c3595c6f4e377359ce269
Nicolás Ibáñez Expósito <nixer1979@gmail.com>
2026-02-20 20:30:47 +0100

    Añadido un script llamado weather

--- install.sh
+++ install.sh
@@ -8,12 +8,14 @@ echo -e "${GREEN}Iniciando instalación de dotfiles... ${NC}"
 
 # Crear carpeta .config si no existe
 mkdir -p ~/.config
-
+mkdir -p ~/.local/bin
 # stow -v (verbose) -R (recursive) -t (target)
 # Linkeamos lo que hay dentro de la carpeta 'config' a ~/.config
+
 stow -v -R -t ~/.config config
+stow -v -R -t ~/.local/bin scripts
 
 # Linkeamos lo que hay en 'shell' al HOME
-# stow -v -R -t ~ shell
+ stow -v -R -t ~ shell
 
 echo -e "${GREEN}¡Todo listo! Enlaces creados.${NC}"
